243c798096f5ceff02fac6f17d0f152ff5b7c8e0
smuddgge <68658429+smuddgge@users.noreply.github.com>
2022-11-25 19:25:39 +0000

    Update tests

--- SquishyYaml/src/test/java/com/github/smuddgge/squishyyaml/TestYamlConfiguration.java
+++ SquishyYaml/src/test/java/com/github/smuddgge/squishyyaml/TestYamlConfiguration.java
@@ -7,16 +7,18 @@ import java.io.File;
 public class TestYamlConfiguration {
 
     @Test
-    public void test() {
+    public void testLoadAndSave() throws Exception {
         YamlConfiguration configuration = new YamlConfiguration(new File("src/main/resources"), "test.yaml");
+
         configuration.load();
-        configuration.set(null);
+        configuration.set("test", 1);
         configuration.save();
 
-        configuration.getSection("test.test").set("testing");
+        YamlConfiguration configuration2 = new YamlConfiguration(new File("src/main/resources"), "test.yaml");
+        configuration2.load();
 
-        System.out.println(configuration.get("test.test"));
-
-        configuration.save();
+        if ((int) configuration2.get("test") != 1) {
+            throw new Exception();
+        }
     }
 }
